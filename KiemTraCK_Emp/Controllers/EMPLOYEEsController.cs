930976ef7dde2289288372dd9155109e8e46244d
kimochikawai <truongnxth1805028@fpt.edu.vn>
2019-11-28 10:36:14 +0700

    dsass

---
+++ KiemTraCK_Emp/Controllers/EMPLOYEEsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KiemTraCK_Emp.Models;
+
+namespace KiemTraCK_Emp.Controllers
+{
+    public class EMPLOYEEsController : Controller
+    {
+        private KiemTra_Emp_2Entities db = new KiemTra_Emp_2Entities();
+
+        // GET: EMPLOYEEs
+        public ActionResult Index()
+        {
+            return View(db.EMPLOYEEs.ToList());
+        }
+
+        // GET: EMPLOYEEs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEEs.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EMPLOYEEs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,EMPLOYEEID,EMPLOYEENAME,DEPARTMENT,SALARY")] EMPLOYEE eMPLOYEE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EMPLOYEEs.Add(eMPLOYEE);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEEs.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // POST: EMPLOYEEs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,EMPLOYEEID,EMPLOYEENAME,DEPARTMENT,SALARY")] EMPLOYEE eMPLOYEE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eMPLOYEE).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEEs.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // POST: EMPLOYEEs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EMPLOYEE eMPLOYEE = db.EMPLOYEEs.Find(id);
+            db.EMPLOYEEs.Remove(eMPLOYEE);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ KiemTraCK_Emp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KiemTraCK_Emp.MvcApplication" Language="C#" %>
